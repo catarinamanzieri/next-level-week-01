137e420263e60eb419ee5e54feb51603a2356387
catarinamanzieri <catarina.manzieri@gmail.com>
2020-06-07 08:06:04 -0300

    Criar função para selecionar e deselecionar os itens no cadastro

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -33,9 +33,9 @@ function getCities(event) {
     fetch(url)
     .then( res => res.json() )
     .then( cities => {
-        
+
         for( const city of cities ) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
 
         citySelect.disabled = false
@@ -48,3 +48,52 @@ function getCities(event) {
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+
+// Itens de coleta
+// pegar todos os li's
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+
+    // Adicionar ou remover uma classe com javascript
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    // Verificar se existem itens selecionados, se sim
+    //pegar os itens selecionados
+
+    const alreadySelected = selectedItems.findIndex( item => {
+        const itemFound = item == itemId // isso será true ou false
+        return itemFound
+    })
+
+    // se já estiver selecionado,
+    if ( alreadySelected >= 0 ) {
+        // tirar da seleção
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferent = item != itemId // false
+            return itemIsDifferent
+        })
+
+        selectedItems = filteredItems
+    } else {
+        // se não estiver selecionado, 
+        // adicionar à seleção
+        selectedItems.push(itemId)
+    }
+    
+        // atualizar o campo escondido com os itens selecionados
+        collectedItems.value = selectedItems
+}
